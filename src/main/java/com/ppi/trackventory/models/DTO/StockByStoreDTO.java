5bb8ad876e9ceef52617f39cec448264d109611c
Andres Sossa <kevin.sossa@hotmail.com>
2025-05-26 22:47:07 -0500

    Interface Implementation

--- src/main/java/com/ppi/trackventory/models/DTO/StockByStoreDTO.java
+++ src/main/java/com/ppi/trackventory/models/DTO/StockByStoreDTO.java
@@ -1,70 +1,15 @@
 package com.ppi.trackventory.models.DTO;
 
-public class StockByStoreDTO {
-    private String reference;
-    private String quantity;
-    private String nameColor;
-    private String nameProduct;
-    private String code;
-    private String colorVrt;
+public interface StockByStoreDTO {
+    String getCodeStore();
 
-    public StockByStoreDTO() {
-    }
+    int getStock();
 
-    public StockByStoreDTO(String reference, String quantity, String nameColor, String nameProduct, String code, String colorVrt) {
-        this.reference = reference;
-        this.quantity = quantity;
-        this.nameColor = nameColor;
-        this.nameProduct = nameProduct;
-        this.code = code;
-        this.colorVrt = colorVrt;
-    }
+    String getNameColor();
 
-    public String getReference() {
-        return reference;
-    }
+    String getNameProduct();
 
-    public void setReference(String reference) {
-        this.reference = reference;
-    }
+    String getCodeVariation();
 
-    public String getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(String quantity) {
-        this.quantity = quantity;
-    }
-
-    public String getNameColor() {
-        return nameColor;
-    }
-
-    public void setNameColor(String nameColor) {
-        this.nameColor = nameColor;
-    }
-
-    public String getNameProduct() {
-        return nameProduct;
-    }
-
-    public void setNameProduct(String nameProduct) {
-        this.nameProduct = nameProduct;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getColorVrt() {
-        return colorVrt;
-    }
-
-    public void setColorVrt(String colorVrt) {
-        this.colorVrt = colorVrt;
-    }
+    String getColorVrt();
 }
